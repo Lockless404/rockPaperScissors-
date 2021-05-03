7acfcc50f82fd8bc1bc83eb80bac3a714e76b2da
Hamza <hamzatarar8@gmail.com>
2021-05-03 13:29:37 +0200

    Completed the javascrit part of the game.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,17 +4,36 @@ function computerPlay() {
     return computerResult;
 }
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt('Select Rock, Paper or Scissors.',);
-    computerSelection = computerPlay();
-    if (playerSelection == computerSelection) {
-        let DrawResult = 'Match is a Draw ' + playerSelection + ' and ' + computerSelection + ' are the same. Suck a dick.'
-        return DrawResult;
-    } else if((playerSelection.toUppercase() == 'ROCK' && computerSelection == 'Scissors') || (playerSelection.toUppercase() == 'PAPER' && computerSelection == 'Rock') || (playerSelection.toUppercase() == 'SCISSORS' && computerSelection =='Paper')) {
-        let winResult = 'You won!! ' + playerSelection + ' beats ' + computerSelection + '. You\'re a bigdickboy.';
-        return winResult;
-    } else if ((computerSelection == 'Rock' && playerSelection.toUppercase() == 'SCISSORS') || (computerSelection == 'Paper' && playerSelection.toUppercase() == 'ROCK') || (computerSelection == 'Scissors' && playerSelection.toUppercase() =='PAPER')) {
-        let loseResult = 'You lost ' + computerSelection + ' beats ' + playerSelection + '. You suck, disgraceful performance. Your mummy dont love you no more';
-        return loseResult;
-    }
 
+    for (let i = 0; i <= 6; i++) {
+        computerSelection = computerPlay().toLowerCase();
+        playerSelection = playerSelection.toLowerCase();
+        let computerScore = 0;
+        let playerScore = 0;
+
+        if (playerSelection === computerSelection) {
+            let DrawResult = 'Match is a Draw ' + playerSelection + ' and ' + computerSelection + ' are the same. Suck a dick.'
+            DrawResult;
+        } else if ( (playerSelection === 'rock' && computerSelection === 'scissors') || 
+                (playerSelection === 'paper' && computerSelection === 'rock') || 
+                (playerSelection === 'scissors' && computerSelection ==='paper')) {
+            playerScore = playerScore + 1;
+            let winResult = 'You won!! ' + playerSelection + ' beats ' + computerSelection + '. You\'re a bigdickboy.';
+            winResult;
+        } else if ( (computerSelection === 'rock' && playerSelection === 'scissors') || 
+                (computerSelection === 'paper' && playerSelection === 'rock') || 
+                (computerSelection === 'scissors' && playerSelection ==='paper')) {
+            computerScore = computerScore + 1;
+            let loseResult = 'You lost ' + computerSelection + ' beats ' + playerSelection + '. You suck, disgraceful performance. Your mummy dont love you no more';
+            loseResult;
+        }
+        
+        if (playerScore === 3) {
+            "You win the game. Congratulation.";
+            break;
+        } else if (computerScore === 3) {
+            "You Lose!!! Good day sir.";
+            break;
+        }
+    }      
 }
